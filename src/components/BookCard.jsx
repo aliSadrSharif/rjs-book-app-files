386e5f4904ad83aabd532caf6280a8effc57ddd1
Ali Sadrsharif <alisadrsharif@gmail.com>
2025-03-25 13:45:09 +0330

    icon and addig styles to the bookCard

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+
+import styles from "./BookCard.module.css";
+
+function BookCard({ data: { title, author, image, language, pages } }) {
+  const [like, setLike] = useState(false);
+
+  const likeHandeler = () => {
+    setLike(like => !like);
+  };
+
+  return (
+    <div className={styles.card}>
+      <img src={image} alt={title} />
+      <div className={styles.info}>
+        <h3>{title}</h3>
+        <p>{author}</p>
+        <div>
+          <span>{language}</span>
+          <span>{pages} pages</span>
+        </div>
+      </div>
+      <button onClick={likeHandeler}>
+        <FaHeart color={like ? "red" : "#e0e0e0"} fontSize="1.8rem" />
+      </button>
+    </div>
+  );
+}
+
+export default BookCard;
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,9 +1,16 @@
+import styles from "./Layout.module.css"
+
 function Layout({children}) {
   return (
     <>
-      <header>header</header>
+      <header className={styles.header}>
+        <h1>Book App</h1>
+        <p><a href="https://www.linkedin.com/in/ali-sadrsharif-525586348/">Ali Sadrsharif</a> | Book App</p>
+      </header>
       {children}
-      <footer>footer</footer>
+      <footer className={styles.footer}>
+        <p>Contact me via <a href="">Linkdin</a> or <a href="mailto:alisadrsharif@gmail.com" >Email</a></p>
+      </footer>
     </>
   );
 }
